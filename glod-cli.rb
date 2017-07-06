c17292f6e886f3589a0031ceb63241f30e498b5c
Han Ngo <nntruonghan@gmail.com>
2017-07-06 19:02:18 +0700

    Update sha256 for glod-cli

--- glod-cli.rb
+++ glod-cli.rb
@@ -1,11 +1,11 @@
 class GlodCli < Formula
   homepage "https://github.com/dwarvesf/glod-cli"
   version "1.1.0"
-  url "https://github.com/dwarvesf/glod-cli/releases/download/#{version}/macOS"
-  sha256 "1826e8b5398f10a12d5f315a9f5a670f05ac3e0f6ead7c4edddf621c2260ae6c"
+  url "https://github.com/dwarvesf/glod-cli/releases/download/#{version}/glod-cli_darwin_amd64"
+  sha256 "005b631ac3eb010dba2ef7e3e24e601c06fbd073f557b7a29b60a81ed6467153"
 
   def install
-    system "mv macOS glod"
+    system "mv glod-cli_darwin_amd64 glod"
     bin.install "glod"
   end
 end
