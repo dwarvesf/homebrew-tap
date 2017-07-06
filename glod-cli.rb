b98f959015b6a291ac3c21b57ee4764399449f51
Han Ngo <nntruonghan@gmail.com>
2017-07-06 20:56:47 +0700

    Temporary remove other formula

--- glod-cli.rb
+++ glod-cli.rb
@@ -1,6 +1,6 @@
 class GlodCli < Formula
   homepage "https://github.com/dwarvesf/glod-cli"
-  version "1.1.0"
+  version "v1.1.0"
   url "https://github.com/dwarvesf/glod-cli/releases/download/#{version}/glod-cli_darwin_amd64"
   sha256 "005b631ac3eb010dba2ef7e3e24e601c06fbd073f557b7a29b60a81ed6467153"
 
