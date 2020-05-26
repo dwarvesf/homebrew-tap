bf396a70f953f707a8864ed8056d2799acacd773
phucld <phucld@dwarvesv.com>
2020-05-26 10:27:14 +0700

    Update vimmotion cask

--- Casks/vimmotion.rb
+++ Casks/vimmotion.rb
@@ -1,6 +1,6 @@
 cask 'vimmotion' do
-  version '1.1'
-  sha256 '6fe0a4deea465b04870e7e74a0b8bbcbc2178dce23ba6b57bfff4aecbb2bd360'
+  version '1.1.4'
+  sha256 'd568d4eafea263267dfe2a523733f68ccae0b69aba274fd90f07b2545511d3be'
 
   url "https://github.com/dwarvesf/VimMotionApp/releases/download/v#{version}/VimMotion.#{version}.dmg"
   appcast 'https://github.com/dwarvesf/VimMotionApp/releases.atom'
