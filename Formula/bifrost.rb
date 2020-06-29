c796c433d73d2ce21cfa8d88d6d8f1f967a8330e
fortressbot <fortressbot@dwarvesv.com>
2020-06-29 15:41:59 +0700

    Brew formula update for bifrost version v0.7.5

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -2,16 +2,16 @@
 class Bifrost < Formula
   desc "The CLI tools for Fortress"
   homepage "https://d.foundation"
-  version "0.7.4"
+  version "0.7.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://storage.googleapis.com/dwarvesf/bifrost/v0.7.4/bifrost_darwin_amd64.tar.gz"
-    sha256 "259aab8b875954b719983e8ffa4c890e11921cc0ba412a5acbb1d65225475ced"
+    url "https://storage.googleapis.com/dwarvesf/bifrost/v0.7.5/bifrost_darwin_amd64.tar.gz"
+    sha256 "8c77e5e28330f8e1d9537d8b85afba00164495454ca0d36e55e65dd31d29a6fc"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://storage.googleapis.com/dwarvesf/bifrost/v0.7.4/bifrost_linux_amd64.tar.gz"
-      sha256 "9f044c99377bd9cc0450926f38ce48f3029898b6b1f399e0ac2026fe138f32dc"
+      url "https://storage.googleapis.com/dwarvesf/bifrost/v0.7.5/bifrost_linux_amd64.tar.gz"
+      sha256 "c89955f92dd56e6031cece513466a0d15d43a0760e5d2c5e6e9c34be22bafd79"
     end
   end
 
