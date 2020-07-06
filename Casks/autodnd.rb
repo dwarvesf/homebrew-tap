236e60b5d6f0cf7ebb2eef50be362f76afba1918
phucld <phucld@dwarvesv.com>
2020-07-06 13:22:09 +0700

    Update autodnd cask

--- Casks/autodnd.rb
+++ Casks/autodnd.rb
@@ -1,6 +1,6 @@
 cask 'autodnd' do
   version '1.0'
-  sha256 '3d57be05370751c31f9d2e193129b8314883f378a6ce02cc1b494a03e9152f5'
+  sha256 '03d57be05370751c31f9d2e193129b8314883f378a6ce02cc1b494a03e9152f5'
 
   url "https://github.com/dwarvesf/auto-dnd/releases/download/v#{version}/Auto.DND.#{version}.dmg"
   appcast 'https://github.com/dwarvesf/auto-dnd/releases.atom'
