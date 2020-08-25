80160d30e34442fd66be460ea59ed62556d97579
Yuri Pieters <magejohnyjtp@gmail.com>
2020-08-25 10:14:13 +0100

    Update VimMotion to the latest release, 1.2.2

--- Casks/vimmotion.rb
+++ Casks/vimmotion.rb
@@ -1,8 +1,8 @@
 cask 'vimmotion' do
-  version '1.1.4'
-  sha256 'd568d4eafea263267dfe2a523733f68ccae0b69aba274fd90f07b2545511d3be'
+  version '1.2.2'
+  sha256 'b7b86c22c9dc16eb75ee7982d5a45ab5c55ab7f14948ece46ec00387a9ce0703'
 
-  url "https://github.com/dwarvesf/VimMotionApp/releases/download/v#{version}/VimMotion.#{version}.dmg"
+  url "https://github.com/dwarvesf/VimMotionApp/releases/download/v#{version}/VimMotion.#{version.major_minor}.dmg"
   appcast 'https://github.com/dwarvesf/VimMotionApp/releases.atom'
   name 'VimMotion'
   homepage 'https://github.com/dwarvesf/VimMotionApp/'
